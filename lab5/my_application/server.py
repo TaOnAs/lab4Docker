046adfce79eb5904ffcbd293d9cb4d62d60a747c
TaOnAs <mark-ohara@live.ie>
2015-10-14 14:39:22 +0000

    Added app.route

--- lab5/my_application/server.py
+++ lab5/my_application/server.py
@@ -2,8 +2,20 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
+def index:
+    return 'Index File'
+
+@app.route("/hello")
 def hello():
-    return "Hello World!"
+    return 'Hello World'
+
+@app.route("/user/paul")
+def paul
+    return 'User paul'
+
+@app.route("/post/80")
+def port
+    return 'Post 80'
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0",port=8080, debug = True)
